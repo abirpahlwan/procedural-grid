89f4e4f5b4eae8c2ccdd62bf9b0859253aceff78
Pahlwan Rabiul Abir <abir978@gmail.com>
2019-01-17 03:09:29 +0600

    Generating Additonal Vertex Data

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -11,7 +11,7 @@ public class Grid : MonoBehaviour {
 	private Mesh mesh;
 
 	void Awake() {
-		StartCoroutine(Generate());
+		Generate();
 	}
 
 	// Use this for initialization
@@ -24,7 +24,7 @@ public class Grid : MonoBehaviour {
 		
 	}
 
-	void OnDrawGizmos() {
+	/*void OnDrawGizmos() {
 		if (vertices == null) {
 			return;
 		}
@@ -32,28 +32,43 @@ public class Grid : MonoBehaviour {
 		foreach (var vertex in vertices) {
 			Gizmos.DrawSphere(vertex, 0.1f);
 		}
-	}
+	}*/
 
-	private IEnumerator Generate() {
+	private void Generate() {
 		WaitForSeconds wait = new WaitForSeconds(0.05f);
 
 		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
 		mesh.name = "Procedural Grid";
 		
 		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		Vector2[] uv = new Vector2[vertices.Length];
+		Vector4[] tangents = new Vector4[vertices.Length];
+		Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+		
 		for (int i = 0, y = 0; y <= ySize; y++) {
 			for (int x = 0; x <= xSize; x++, i++) {
 				vertices[i] = new Vector3(x, y);
-				yield return wait;
+				uv[i] = new Vector2((float) x / xSize, (float) y / ySize);
+				tangents[i] = tangent;
 			}
 		}
 
 		mesh.vertices = vertices;
+		mesh.uv = uv;
+		mesh.tangents = tangents;
 
-		int[] triangles = new int[3];
-		triangles[0] = 0;
-		triangles[1] = 1;
-		triangles[2] = xSize + 1;
-		mesh.triangles = triangles;
+		int[] triangles = new int[xSize * ySize * 6];
+		
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+
+				mesh.triangles = triangles;
+				mesh.RecalculateNormals();
+			}
+		}
 	}
 }
